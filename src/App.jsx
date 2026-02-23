540bfe1974162c085a2b162b7c640cc18bc2657f
Jônatas Oliveira da Silva <OliveiraJonatas@Outlook.com.br>
2026-02-23 10:54:17 -0300

    feat: adiciona o componente principal da aplicação

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import Navbar from "../src/components/Navbar/Navbar";
+import Footer from "../src/components/Footer/Footer";
+
+import "../src/global.scss";
+
+function App() {
+  return (
+    <>
+      <Navbar />
+      <Footer />
+    </>
+  );
+}
+
+export default App;
