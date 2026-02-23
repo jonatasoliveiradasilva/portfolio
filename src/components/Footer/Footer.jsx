689131a8356011300f363d855b795c8aef564780
Jônatas Oliveira da Silva <OliveiraJonatas@Outlook.com.br>
2026-02-23 10:58:29 -0300

    feat: adiciona o componente footer

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,11 @@
+import styles from "../Footer/Footer.module.scss";
+
+function Footer() {
+    return (
+        <footer className={styles.rodape}>
+            <p>Desenvolvido por Jônatas Oliveira da Silva</p>
+        </footer>
+    );
+}
+
+export default Footer;
